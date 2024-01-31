f8bf832027f23b514227a725b4a375bf35260819
tjasa <tjasa.zilavec@gmail.com>
2024-01-31 21:40:21 +0100

    .simple chart styling and form functionality -> pushing new data into an array and showing in the chart

--- src/app/components/data-form/data-form.component.ts
+++ src/app/components/data-form/data-form.component.ts
@@ -13,15 +13,25 @@ export class DataFormComponent {
   };
   chartedData = [
     { date: new Date(2024, 0, 1), temperature: 10 },
-    { date: new Date(2024, 1, 1), temperature: 12 },
-    { date: new Date(2024, 2, 1), temperature: 11 },
-    { date: new Date(2024, 3, 1), temperature: 13 },
-    { date: new Date(2024, 4, 1), temperature: 15 },
-    { date: new Date(2024, 5, 1), temperature: 7 },
-    { date: new Date(2024, 6, 1), temperature: 2 },
-    { date: new Date(2024, 7, 1), temperature: -1 },
-    { date: new Date(2024, 8, 1), temperature: -3 },
-    { date: new Date(2024, 9, 1), temperature: 5 },
+    { date: new Date(2024, 0, 2), temperature: 12 },
+    { date: new Date(2024, 0, 3), temperature: 11 },
+    { date: new Date(2024, 0, 4), temperature: 13 },
+    { date: new Date(2024, 0, 5), temperature: 15 },
+    { date: new Date(2024, 0, 6), temperature: 7 },
+    { date: new Date(2024, 0, 7), temperature: 2 },
+    { date: new Date(2024, 0, 8), temperature: -1 },
+    { date: new Date(2024, 0, 9), temperature: -3 },
+    { date: new Date(2024, 0, 10), temperature: -5 },
+    { date: new Date(2024, 0, 11), temperature: -3 },
+    { date: new Date(2024, 0, 12), temperature: -2 },
+    { date: new Date(2024, 0, 13), temperature: 1 },
+    { date: new Date(2024, 0, 14), temperature: 3 },
+    { date: new Date(2024, 0, 15), temperature: -5 },
+    { date: new Date(2024, 0, 16), temperature: 5 },
+    { date: new Date(2024, 0, 17), temperature: 6 },
+    { date: new Date(2024, 0, 18), temperature: 3 },
+    { date: new Date(2024, 0, 19), temperature: 6 },
+    { date: new Date(2024, 0, 20), temperature: 10 },
   ];
   submitButtonOptions = {
     text: 'Submit the Data',
@@ -31,9 +41,14 @@ export class DataFormComponent {
   handleSubmit = (e: any) => {
     setTimeout(() => {
       this.successMessage = 'Successfully submitted';
-      //alert('Submitted');
+      this.chartedData.push(this.temperatures);
+      this.temperatures.temperature = 0;
+      this.temperatures.date = new Date();
     }, 1000);
 
     e.preventDefault();
+    setTimeout(() => {
+      this.successMessage = '';
+    }, 3000);
   };
 }
